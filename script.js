54eef2bf715ba3c8b6195bf84e71ad5b863afd15
mohammedjaseer57-del <mohammedjaseer57@gmail.com>
2025-09-10 16:09:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Contact form alert
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  alert("Thank you for contacting me! I will get back to you soon.");
+
+  this.reset();
+
+});
